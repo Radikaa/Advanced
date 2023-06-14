b66dae88688335ab1cd0525ed61d1cd3043480cb
Radoslav Stanchev <stanchev.radoslav89@gmail.com>
2023-06-14 16:58:36 +0300

    up

---
+++ src/AB_Exams_info/TASK_02_Treasure_hunt.java
@@ -0,0 +1,70 @@
+package AB_Exams_info;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TASK_02_Treasure_hunt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] dimensions = scanner.nextLine().split(" ");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+        char[][] matrix = new char[rows][cols];
+
+        int postitionRow = -1;
+        int positionCol = -1;
+
+        for (int r = 0; r < rows; r++) {
+            String[] rowsInput = scanner.nextLine().split("\\s+");
+            for (int c = 0; c < cols; c++) {
+                char currentSymbol = rowsInput[c].charAt(0);
+                matrix[r][c] = currentSymbol;
+                if (currentSymbol == 'Y') {
+                    postitionRow = r;
+                    positionCol = c;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        String command = scanner.nextLine();
+
+        while (!command.equals("Finish")) {
+            switch (command) {
+                case "left":
+                    if (positionCol - 1 >= 0 && matrix[postitionRow][positionCol - 1] != 'T') {
+                        positionCol--;
+                        sb.append(command + " ");
+                    }
+                    break;
+                case "right":
+                    if (positionCol + 1 < cols && matrix[postitionRow][positionCol + 1] != 'T') {
+                        positionCol++;
+                        sb.append(command + " ");
+                    }
+                    break;
+                case "up":
+                    if (postitionRow - 1 >= 0 && matrix[postitionRow - 1][positionCol] != 'T') {
+                        postitionRow--;
+                        sb.append(command + " ");
+                    }
+                    break;
+                case "down":
+                    if (postitionRow + 1 < rows && matrix[postitionRow + 1][positionCol] != 'T') {
+                        postitionRow++;
+                        sb.append(command + " ");
+                    }
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+
+        if (matrix[postitionRow][positionCol] != 'X') {
+            System.out.println("The map is fake!");
+        } else {
+            System.out.println("I've found the treasure!");
+            String[] stepsFormat = sb.toString().split(" ");
+            String format = String.join(", ", stepsFormat);
+            System.out.printf("The right path is " + format);
+        }
+    }
+}
---
+++ src/AB_Exams_info/Task_01_KAT.java
@@ -0,0 +1,53 @@
+package AB_Exams_info;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_01_KAT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<Integer> queueLicense = new ArrayDeque<>();
+        ArrayDeque<Integer> stackCars = new ArrayDeque<>();
+        //Queue- offer, poll, peek
+        //Stack - push, pop, peek
+        Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(queueLicense::offer);
+        Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).forEach(stackCars::push);
+
+        int dayCount = 0;
+        int registeredCars = 0;
+
+        while (queueLicense.size() > 0 && stackCars.size() > 0) {
+            dayCount++;
+            int currentNumberOfPlate = queueLicense.poll();
+            int currentNumberOfCars = stackCars.pop();
+
+            if (currentNumberOfPlate > (currentNumberOfCars * 2)) {
+                queueLicense.addLast(currentNumberOfPlate - (currentNumberOfCars * 2));
+                registeredCars += currentNumberOfCars;
+            } else if (currentNumberOfPlate < (currentNumberOfCars * 2)) {
+                int leftCars = currentNumberOfCars - (currentNumberOfPlate / 2);
+                stackCars.addLast(leftCars);
+                registeredCars += (currentNumberOfPlate / 2);
+            } else {
+                registeredCars += currentNumberOfCars;
+            }
+        }
+        System.out.printf("%d cars were registered for %d days!%n", registeredCars, dayCount);
+        if (!queueLicense.isEmpty()) {
+            long sumOfUnplacedPlates = 0;
+            while (!queueLicense.isEmpty()) {
+                sumOfUnplacedPlates += queueLicense.poll();
+            }
+            System.out.printf("%d license plates remain!", sumOfUnplacedPlates);
+        } else if (!stackCars.isEmpty()) {
+            long sumOfCarsWithoutPlate = 0;
+            while (!stackCars.isEmpty()) {
+                sumOfCarsWithoutPlate += stackCars.pop();
+            }
+            System.out.printf("%d cars remain without license plates!", sumOfCarsWithoutPlate);
+        } else {
+            System.out.println("Good job! There is no queue in front of the KAT!");
+        }
+    }
+}
